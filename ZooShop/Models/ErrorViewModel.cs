63eefc388ece8608b15f7f3a299cdcc9b2b16156
Venelin Krastev <venelinkrstev@gmail.com>
2024-11-06 19:02:46 +0200

    Add project files.

---
+++ ZooShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ZooShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
