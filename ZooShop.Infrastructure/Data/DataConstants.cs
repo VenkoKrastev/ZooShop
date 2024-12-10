867f5f89d76480a5deb03fd83d25df33279967a6
Venelin Krastev <venelinkrstev@gmail.com>
2024-12-10 12:40:54 +0200

    Make Models

---
+++ ZooShop.Infrastructure/Data/DataConstants.cs
@@ -0,0 +1,36 @@
+﻿namespace ZooShop.Infrastructure.Data
+{
+    public static class DataConstants
+    {
+        public const int ProductNameMinLength = 3;
+        public const int ProductNameMaxLength = 100;
+
+        public const int ProductDetailsMinLength = 10;
+        public const int ProductDetailsMaxLength = 1000;
+
+        public const int ProductImageUrlMinLength = 5;
+        public const int ProductImageUrlMaxLength = 1000000000;
+
+        public const string ProductPriceMinValue = "0";
+        public const string ProductPriceMaxValue = "100000";
+
+        public const int CustomerNameMinLength = 3;
+        public const int CustomerNameMaxLength = 50;
+
+        public const int CustomerPhoneNumberMinLength = 8;
+        public const int CustomerPhoneNumberMaxLength = 20;
+
+        public const int CustomerEmailMinLength = 7;
+        public const int CustomerEmailMaxLength = 100;
+
+        public const int AnimalNameMinLength = 3;
+        public const int AnimalNameMaxLength = 30;
+
+        public const int ProductCategoryMinLength = 3;
+        public const int ProductCategoryMaxLength = 50;
+
+        public const string LengthErrorMessage = "{0} must be between {2} and {1} characters long!";
+        public const string RangeErrorMessage = "{0} must be a number between {1} and {2}!";
+
+    }
+}
---
+++ ZooShop.Infrastructure/Data/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooShop.Infrastructure.Data.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string CustomerEmail { get; set; }  
+
+        public List<OrderItem> Items { get; set; }
+
+        public DateTime OrderDate  { get; set; } = DateTime.Now;
+    }
+}
