dc77c7595170fee689b8f7958a3fc71f4ee9b90a
Venelin Krastev <venelinkrstev@gmail.com>
2024-12-15 15:28:41 +0200

    add dogcontroller

---
+++ ZooShop.Core/Models/DogCategoryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZooShop.Infrastructure.Data.Models;
+
+namespace ZooShop.Core.Models
+{
+    public class DogCategoryViewModel
+    {
+        public IEnumerable<Product> DogFood { get; set; }
+        public IEnumerable<Accessory> DogAccessories { get; set; }
+        public IEnumerable<Product> AllDogProducts { get; set; }
+    }
+}
