87335f1d3e6c42004f6f79a3838098332de06a7f
Venelin Pavlinov Krastev <122910303+VenkoKrastev@users.noreply.github.com>
2024-12-17 22:03:38 +0200

    add order controller

---
+++ ZooShop/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ZooShop.Infrastructure.Data;
+
+namespace ZooShop.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ZooShopDbContext _context;
+
+        public OrderController(ZooShopDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var orders = await _context.Orders.Include(o => o.User).Include(o => o.Items).ThenInclude(i => i.Product).ToListAsync();
+            return View(orders);
+        }
+
+    }
+}
